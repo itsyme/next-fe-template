0c93e8cfeb1e725cb3525f6f0f31c3ace691bfb4
unknown <e0524723@u.nus.edu>
2024-01-19 13:18:02 +0800

    feat: add login dialog

--- components/header.tsx
+++ components/header.tsx
@@ -10,32 +10,55 @@ import {
     navigationMenuTriggerStyle,
   } from "@/components/ui/navigation-menu"  
 import Link from "next/link";
+import { useState } from "react";
+import { Button } from "./ui/button";
+import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from "./ui/dialog";
 
 const Header = () => {
     return (
-        <div className="p-1 flex justify-center">
+        <div className="p-2 flex justify-center">
             <NavigationMenu>
                 <NavigationMenuList>
                     <NavigationMenuItem>
                         <Link href="/">
-                            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                            <NavigationMenuLink className={navigationMenuTriggerStyle() + " px-5"}>
                                 <text className="text-lg">
                                     Home
                                 </text>
                             </NavigationMenuLink>
                         </Link>
                         <Link href="/about">
-                            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                            <NavigationMenuLink className={navigationMenuTriggerStyle() + " px-5"}>
                                 <text className="text-lg">
                                     About
                                 </text>
                             </NavigationMenuLink>
                         </Link>
+                        <Dialog>
+                            <DialogTrigger className="px-5">
+                                <text className="text-lg">
+                                    Log In
+                                </text>
+                            </DialogTrigger>
+                            <DialogContent>
+                                <DialogHeader>
+                                    <DialogTitle>
+                                        <text className="text-lg">
+                                            Log In
+                                        </text>
+                                    </DialogTitle>
+                                </DialogHeader>
+                                <div className="py-6 w-full">
+                                    <Button className="w-full">
+                                        Login with Google
+                                    </Button>
+                                </div>
+                            </DialogContent>
+                        </Dialog>
                     </NavigationMenuItem>
                 </NavigationMenuList>
-            </NavigationMenu>   
+            </NavigationMenu>
         </div>
-        
     )
 }
 
